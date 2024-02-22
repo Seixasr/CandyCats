7b09b58ea9fbd77d70c8f20fde6a218fa879447e
Seixasr <rafaelseixas.figueiredo@gmail.com>
2024-02-22 20:34:49 -0300

    dificuldade corrigida

---
+++ Navigation Screen/AboutMeView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutMeView.swift
+//  TestWwdc
+//
+//  Created by Rafael Santos Seixas Figueiredo on 22/02/24.
+//
+
+import SwiftUI
+
+struct AboutMeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutMeView()
+}
